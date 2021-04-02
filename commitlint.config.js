6b6dbcd09f2b42db3aeba3226cfc3510151fbd30
amber <amberenjoy@outlook.com>
2021-04-02 21:17:09 +0800

    CI: Complete custom commitLint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,30 @@
 module.exports = {
-    /*
-    * Resolve and load @commitlint/config-conventional from node_modules.
-    * Referenced packages must be installed
-    */
-      extends: ['@commitlint/config-conventional'],
-      /*
-       * Any rules defined here will override rules from @commitlint/config-conventional
-       */
-      rules: {
-          'type-enum': [
-              2,
-              'always',
-              ['build', 'chore', 'ci', 'docs', 'improvement', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
-          ],
-      },
-  };
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    "subject-case": [2, "always", ["upper-case"]],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "Build",
+        "Chore",
+        "CI",
+        "Docs",
+        "Improvement",
+        "Feat",
+        "Fix",
+        "Refactor",
+        "Revert",
+        "Style",
+        "Test",
+      ],
+    ],
+  },
+};
